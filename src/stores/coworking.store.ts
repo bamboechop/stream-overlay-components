9690aacd798daa6b4b1cb927866339e2e77881b6
bamboechop <info@bamboechop.at>
2024-05-26 00:19:03 +0200

    refactor: refs for my notes and user notes
    With just a notes ref and two computeds based on it
    the splide slider would go and refresh whenever I add
    a note for myself and not only when a user note is added

--- src/stores/coworking.store.ts
+++ src/stores/coworking.store.ts
@@ -1,27 +1,34 @@
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import type { Note } from '@/common/interfaces/notes.interface';
 
 export const useCoworkingStore = defineStore('Coworking', () => {
-  const notes = ref<Note[]>([]);
-
-  const myNotes = computed(() => notes.value.filter(note => note.displayName === 'bamboechop'));
-  const viewerNotes = computed(() => notes.value.filter(note => note.displayName !== 'bamboechop'));
+  const myNotes = ref<Note[]>([]);
+  const viewerNotes = ref<Note[]>([]);
 
   const removeNotesFromUser = (displayName: string) => {
-    notes.value = notes.value.filter(n => n.displayName !== displayName);
+    viewerNotes.value = viewerNotes.value.filter(n => n.displayName !== displayName);
   };
 
   const updateNote = (note: Note) => {
     if (note.status === 'in-progress') {
-      notes.value.push(note);
+      if (note.displayName === 'bamboechop') {
+        myNotes.value.push(note);
+      } else {
+        viewerNotes.value.push(note);
+      }
     } else {
-      notes.value = notes.value.filter(n => n.note_id !== note.note_id);
+      if (note.displayName === 'bamboechop') {
+        myNotes.value = myNotes.value.filter(n => n.note_id !== note.note_id);
+      } else {
+        viewerNotes.value = viewerNotes.value.filter(n => n.note_id !== note.note_id);
+      }
     }
   };
 
   const setNotes = (notesToSet: Note[]) => {
-    notes.value = notesToSet;
+    myNotes.value = notesToSet.filter(note => note.displayName === 'bamboechop');
+    viewerNotes.value = notesToSet.filter(note => note.displayName !== 'bamboechop');
   };
 
   return {
