b8cd5aea0e34c91930524aa529658dbad12fc723
bamboechop <info@bamboechop.at>
2024-05-31 20:57:25 +0200

    refactor: load obs composable only in taskbar view

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,10 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 import { useSearchParamsComposable } from '@/composables/theme.composable';
-import { useObsComposable } from '@/composables/obs.composable';
 
 const { theme } = useSearchParamsComposable();
 
 onMounted(async () => {
   document.documentElement.dataset.theme = theme.value;
-
-  await useObsComposable();
 });
 </script>
--- src/views/TaskBarView.vue
+++ src/views/TaskBarView.vue
@@ -14,11 +14,13 @@ import Windows95Theme from '@/components/task-bar/windows95/TaskBar.vue';
 import { useTwitchStreamInfo } from '@/composables/twitch-stream-info.composable';
 import { useSearchParamsComposable } from '@/composables/theme.composable';
 import { useApplicationStore } from '@/stores/application.store';
+import { useObsComposable } from '@/composables/obs.composable';
 
 const { theme } = useSearchParamsComposable();
 
 const applicationStore = useApplicationStore();
 const { activeApplications } = storeToRefs(applicationStore);
 
+useObsComposable();
 useTwitchStreamInfo();
 </script>
