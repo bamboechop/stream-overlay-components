e62a55ec88f3590991186f494b986dd7969d7a51
bamboechop <info@bamboechop.at>
2024-12-26 13:42:38 +0100

    feat: update the last application to be active

--- src/stores/application.store.ts
+++ src/stores/application.store.ts
@@ -5,12 +5,30 @@ import type { IProgram } from '@/components/task-bar/task-bar.interface';
 export const useApplicationStore = defineStore('Application Store', () => {
   const activeApplications = ref<IProgram[]>([]);
 
+  const updateActiveApplication = () => {
+    activeApplications.value = activeApplications.value.map((application, index) => {
+      if (index === activeApplications.value.length - 1) {
+        return {
+          ...application,
+          active: true,
+        };
+      }
+
+      return {
+        ...application,
+        active: false,
+      };
+    });
+  };
+
   const addActiveApplication = (application: IProgram) => {
     activeApplications.value.push(application);
+    updateActiveApplication();
   };
 
   const removeActiveApplication = (id: string) => {
     activeApplications.value = activeApplications.value.filter(activeApplication => activeApplication.id !== id);
+    updateActiveApplication();
   };
 
   const removeActiveApplications = () => {
