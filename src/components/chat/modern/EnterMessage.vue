84e25582d364d5d8278c8e17e1fa2f52b4208eb4
bamboechop <info@bamboechop.at>
2024-12-26 11:57:32 +0100

    refactor: EnterMessage now overflow ellipsis the text when the window gets too small

--- src/components/chat/modern/EnterMessage.vue
+++ src/components/chat/modern/EnterMessage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="enter-message">
-    <span>Nachricht <template v-if="viewers > 0">an {{ viewers }} Person<template v-if="viewers !== 1">en</template></template> schreiben...</span>
-    <SendHorizontal :size="18" />
+    <span class="enter-message__text">Nachricht <template v-if="viewers > 0">an {{ viewers }} Person<template v-if="viewers !== 1">en</template></template> schreiben</span>
+    <SendHorizontal
+      class="enter-message__icon"
+      :size="18" />
   </div>
 </template>
 
@@ -21,10 +23,18 @@ const { viewers } = storeToRefs(store);
   align-items: center;
   background-color: rgba(14, 14, 16, 0.5);
   border-radius: $window-frame-border-radius - $window-frame-padding;
-  display: flex;
-  flex-direction: row;
+  column-gap: 4px;
+  display: grid;
   font-size: 14px;
+  grid-template-columns: 1fr 18px;
   justify-content: space-between;
   padding: 8px;
+
+  &__text {
+    overflow: hidden;
+    text-align: left;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 }
 </style>
