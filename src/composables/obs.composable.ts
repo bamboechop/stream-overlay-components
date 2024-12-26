9ed342629d95c71a11627f26970451744a9ff926
bamboechop <info@bamboechop.at>
2024-12-26 13:34:04 +0100

    feat: move program information to own composable

--- src/composables/obs.composable.ts
+++ src/composables/obs.composable.ts
@@ -1,11 +1,10 @@
 import OBSWebSocket from 'obs-websocket-js';
-import { computed, onBeforeUnmount, ref, watch } from 'vue';
+import { onBeforeUnmount, ref, watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import type { TProgramId } from '@/common/types/index.type';
 import { useApplicationStore } from '@/stores/application.store';
-import type { IProgram } from '@/components/task-bar/task-bar.interface';
-import { useSearchParamsComposable } from '@/composables/theme.composable';
 import { useTwitchStore } from '@/stores/twitch.store';
+import { useProgramInformationComposable } from '@/composables/program-information.composable';
 
 const obsSceneIdToProgramIdMapping: { [sceneId: number]: TProgramId } = {
   4: 'intermission',
@@ -34,94 +33,10 @@ export async function useObsComposable() {
   const { activeApplications } = storeToRefs(applicationStore);
   const { addActiveApplication, removeActiveApplication, removeActiveApplications } = applicationStore;
 
-  const { theme, themePath } = useSearchParamsComposable();
+  const { iconPath, programInformation } = useProgramInformationComposable();
 
   const twitchStore = useTwitchStore();
   const { category } = storeToRefs(twitchStore);
-
-  const iconPath = computed(() => {
-    if (theme.value !== 'modern') {
-      return `/programs/${themePath}/player.icon.png`;
-    }
-
-    switch (category.value) {
-      case 'Bloons TD 6':
-        return `/programs/${themePath}/bloons-td-6.icon.png`;
-      case 'Brotato':
-        return `/programs/${themePath}/brotato.icon.png`;
-      case 'Cities: Skylines II':
-        return `/programs/${themePath}/cities-skylines-ii.icon.png`;
-      case 'Cult of the Lamb':
-        return `/programs/${themePath}/cult-of-the-lamb.icon.png`;
-      case 'Dorfromantik':
-        return `/programs/${themePath}/dorfromantik.icon.png`;
-      case 'Deep Rock Galactic: Survivor':
-        return `/programs/${themePath}/drg-survivor.icon.png`;
-      case 'Golf It!':
-        return `/programs/${themePath}/golf-it.icon.png`;
-      case 'Horizon Zero Dawn Remastered':
-        return `/programs/${themePath}/horizon-zero-dawn-remastered.icon.png`;
-      case 'Minecraft':
-        return `/programs/${themePath}/minecraft.icon.png`;
-      case 'Mini Metro':
-        return `/programs/${themePath}/mini-metro.icon.png`;
-      case 'Mini Motorways':
-        return `/programs/${themePath}/mini-motorways.icon.png`;
-      case 'Project Zomboid':
-        return `/programs/${themePath}/project-zomboid.icon.png`;
-      case 'Pokémon Trading Card Game Live':
-        return `/programs/${themePath}/ptcgl.icon.png`;
-      case 'shapez':
-        return `/programs/${themePath}/shapez.icon.png`;
-      case 'Spec Ops: The Line':
-        return `/programs/${themePath}/spec-ops.icon.png`;
-      case 'Stray':
-        return `/programs/${themePath}/stray.icon.png`;
-      case 'Trackmania':
-        return `/programs/${themePath}/trackmania.icon.png`;
-      case 'UNO':
-        return `/programs/${themePath}/uno.icon.png`;
-      case 'URBO: Dream One':
-        return `/programs/${themePath}/urbo.icon.png`;
-      case 'Just Chatting':
-      case 'Media Player':
-      default:
-        return `/programs/${themePath}/player.icon.png`;
-    }
-  });
-
-  const programInformation = ref<Record<TProgramId, IProgram>>({
-    'chat': {
-      active: true,
-      iconPath: `/programs/${themePath}/chat.icon.png`,
-      id: 'chat',
-      text: 'Chat',
-    },
-    'intermission': {
-      active: false,
-      iconPath: `/programs/${themePath}/intermission.icon.png`,
-      id: 'intermission',
-      text: 'Intermission',
-    },
-    'media-player': {
-      active: false,
-      iconPath: iconPath.value,
-      id: 'media-player',
-      text: category.value,
-    },
-    'pcg': {
-      active: false,
-      iconPath: `/programs/${themePath}/pokemon-community-game.icon.png`,
-      id: 'pcg',
-      text: 'Pokémon Community Game',
-    },
-    'webcam': {
-      active: false,
-      iconPath: `/programs/${themePath}/webcam.icon.png`,
-      id: 'webcam',
-      text: 'Webcam',
-    },
-  });
 
   const obs = new OBSWebSocket();
 
