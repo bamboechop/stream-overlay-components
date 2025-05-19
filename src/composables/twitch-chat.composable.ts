e68cc3f9d3006541023b3178c64ea2665069678a
bamboechop <info@bamboechop.at>
2025-05-19 11:14:18 +0200

    fix: refine room ID check for shared Twitch chat messages

--- src/composables/twitch-chat.composable.ts
+++ src/composables/twitch-chat.composable.ts
@@ -214,7 +214,8 @@ export async function useTwitchChat(theme?: TTheme) {
       });
 
       client.on('chat', async (channel: string, userstate: ChatUserstate, message: string, self: boolean) => {
-        if (self || userstate['source-room-id'] !== userstate['room-id']) { // room ids are different in stream together shared chats - only continue when the source and target room ids are the same
+        // room ids are different in stream together shared chats - only continue when the source and target room ids are the same
+        if (self || (userstate['source-room-id'] && userstate['source-room-id'] !== userstate['room-id'])) {
           return;
         }
 
