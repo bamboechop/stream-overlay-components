219aac76491af1c56ca3ecd98da9916d6900350b
bamboechop <info@bamboechop.at>
2025-07-11 21:55:48 +0200

    chore: emote constant file

---
+++ src/common/constants/emotes.constant.ts
@@ -0,0 +1,115 @@
+export const EMOTES: { name: string; new?: boolean; updated?: boolean; url: string }[] = [
+  {
+    name: 'bamboe1Butter',
+    url: '/emotes/bamboe1Butter.png',
+  },
+  {
+    name: 'bamboe1Cool',
+    url: '/emotes/bamboe1Cool.png',
+  },
+  {
+    name: 'bamboe1Cozy',
+    url: '/emotes/bamboe1Cozy.png',
+  },
+  {
+    name: 'bamboe1Dead',
+    updated: true,
+    url: '/emotes/bamboe1Dead.png',
+  },
+  {
+    name: 'bamboe1Derp',
+    url: '/emotes/bamboe1Derp.png',
+  },
+  {
+    name: 'bamboe1KEKW',
+    url: '/emotes/bamboe1KEKW.png',
+  },
+  {
+    name: 'bamboe1Love',
+    url: '/emotes/bamboe1Love.png',
+  },
+  {
+    name: 'bamboe1Lurk',
+    url: '/emotes/bamboe1Lurk.gif',
+  },
+  {
+    name: 'bamboe1Rage',
+    url: '/emotes/bamboe1Rage.png',
+  },
+  {
+    name: 'bamboe1Raid',
+    url: '/emotes/bamboe1Raid.gif',
+  },
+  {
+    name: 'bamboe1Sad',
+    url: '/emotes/bamboe1Sad.png',
+  },
+  {
+    name: 'bamboe1Scared',
+    url: '/emotes/bamboe1Scared.png',
+  },
+  {
+    name: 'bamboe1Sexy',
+    url: '/emotes/bamboe1Sexy.png',
+  },
+  {
+    name: 'bamboe1Bla',
+    new: true,
+    url: '/emotes/bamboe1Bla.gif',
+  },
+  {
+    name: 'bamboe1Bonk',
+    new: true,
+    url: '/emotes/bamboe1Bonk.png',
+  },
+  {
+    name: 'bamboe1Clap',
+    new: true,
+    url: '/emotes/bamboe1Clap.gif',
+  },
+  {
+    name: 'bamboe1Drink',
+    new: true,
+    url: '/emotes/bamboe1Drink.png',
+  },
+  {
+    name: 'bamboe1GG',
+    new: true,
+    url: '/emotes/bamboe1GG.png',
+  },
+  {
+    name: 'bamboe1Innocent',
+    new: true,
+    url: '/emotes/bamboe1Innocent.gif',
+  },
+  {
+    name: 'bamboe1Noted',
+    new: true,
+    url: '/emotes/bamboe1Noted.gif',
+  },
+  {
+    name: 'bamboe1Party',
+    new: true,
+    url: '/emotes/bamboe1Party.gif',
+  },
+  {
+    name: 'bamboe1Pat',
+    new: true,
+    url: '/emotes/bamboe1Pat.png',
+  },
+  {
+    name: 'bamboe1Shy',
+    new: true,
+    url: '/emotes/bamboe1Shy.gif',
+  },
+  {
+    name: 'bamboe1Sleep',
+    new: true,
+    url: '/emotes/bamboe1Sleep.png',
+  },
+  {
+    name: 'bamboe1Steer',
+    new: true,
+    url: '/emotes/bamboe1Steer.gif',
+  },
+];
