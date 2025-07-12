0c07f1f2e261c16e779193c507394ed16cd47d7d
bamboechop <info@bamboechop.at>
2025-07-12 20:06:30 +0200

    chore: move obs composable usage to App.vue

--- src/views/AdView.vue
+++ src/views/AdView.vue
@@ -7,13 +7,11 @@
 <script lang="ts" setup>
 import ModernTheme from '@/components/ad-view/modern/Ad.vue';
 import { useSearchParamsComposable } from '@/composables/search-params.composable';
-import { useObsComposable } from '@/composables/obs.composable';
 import { useTwitchStreamInfoComposable } from '@/composables/twitch-stream-info.composable';
 import { useEventStreamComposable } from '@/composables/event-stream.composable';
 
 const { theme } = useSearchParamsComposable();
 
-useObsComposable();
 useEventStreamComposable();
 useTwitchStreamInfoComposable();
 </script>
--- src/views/TaskBarView.vue
+++ src/views/TaskBarView.vue
@@ -9,12 +9,12 @@
 
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
 import ModernTheme from '@/components/task-bar/modern/TaskBar.vue';
 import Windows95Theme from '@/components/task-bar/windows95/TaskBar.vue';
 import { useTwitchStreamInfoComposable } from '@/composables/twitch-stream-info.composable';
 import { useSearchParamsComposable } from '@/composables/search-params.composable';
 import { useApplicationStore } from '@/stores/application.store';
-import { useObsComposable } from '@/composables/obs.composable';
 import { useEventStreamComposable } from '@/composables/event-stream.composable';
 
 const { theme } = useSearchParamsComposable();
@@ -22,7 +22,6 @@ const { theme } = useSearchParamsComposable();
 const applicationStore = useApplicationStore();
 const { activeApplications } = storeToRefs(applicationStore);
 
-useObsComposable();
 useEventStreamComposable();
 useTwitchStreamInfoComposable();
 </script>
