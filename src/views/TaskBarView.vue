bf56a039154c2c6d4cc15a1cc61137c00a959eeb
bamboechop <info@bamboechop.at>
2026-02-16 16:25:38 +0100

    chore(BMB-91): remove Windows95 Task Bar

--- src/views/TaskBarView.vue
+++ src/views/TaskBarView.vue
@@ -2,15 +2,11 @@
   <template v-if="theme === 'modern'">
     <ModernTheme :programs="activeApplications" />
   </template>
-  <template v-if="theme === 'windows-95'">
-    <Windows95Theme :programs="activeApplications" />
-  </template>
 </template>
 
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
 import ModernTheme from '@/components/task-bar/modern/TaskBar.vue';
-import Windows95Theme from '@/components/task-bar/windows95/TaskBar.vue';
 import { useTwitchStreamInfoComposable } from '@/composables/twitch-stream-info.composable';
 import { useSearchParamsComposable } from '@/composables/search-params.composable';
 import { useApplicationStore } from '@/stores/application.store';
